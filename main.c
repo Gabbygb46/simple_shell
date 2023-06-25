ccadf816626530260c784ff875f6a48bebc8503e
selam <www.selamyibarh@gmail.com>
2023-06-25 06:30:18 -0700

    simple shell

--- main.c
+++ main.c
@@ -1,26 +1,26 @@
 #include "shell.h"
 
 /**
- * main - entry point
+ * main - is a entry point
  * @ac: argument count
  * @av: argument vector
  *
- * Return: success on 0, error on 1
+ * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	int f = 2;
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (f)
-			: "r" (f));
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
-		f = open(av[1], O_RDONLY);
-		if (f == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
@@ -35,7 +35,7 @@ int main(int ac, char **av)
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = f;
+		info->readfd = fd;
 	}
 	populate_env_list(info);
 	read_history(info);
